1eb9a5804fdadbba426e1c2d0382f6154cdf3d11
shynome <shynome@gmail.com>
2024-11-23 18:34:00 +0800

    init capacitor android

---
+++ android/app/src/main/java/net/remoon/salt/MainActivity.java
@@ -0,0 +1,5 @@
+package net.remoon.salt;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'net.remoon.salt',
+  appName: 'salt',
+  webDir: 'build'
+};
+
+export default config;
