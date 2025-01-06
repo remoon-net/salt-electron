5f0535825eb70d47f5c0c762a4656f6343782464
shynome <shynome@gmail.com>
2025-01-06 23:27:04 +0800

    修复 @remoon.net/bootstrap 和此包内引用 bootstrap 打包两次的问题

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import path from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		host: 'salt.lo.remoon.net',
+	},
+	resolve: {
+		alias: {
+			bootstrap: path.resolve(__dirname, 'node_modules', 'bootstrap'),
+		},
 	},
 })
