f421c55474c96fd332e35907997b3d61e1e05620
shynome <shynome@gmail.com>
2025-01-06 23:57:47 +0800

    出错时自动跳回首页

--- src/routes/peer/add/+page.ts
+++ src/routes/peer/add/+page.ts
@@ -1,5 +1,6 @@
 import type { Peer, Status } from '$lib/config'
 import xhe from '$lib/xhe'
+import { redirect } from '@sveltejs/kit'
 
 export async function load({ url }) {
 	const s = await xhe.get('status')
@@ -7,8 +8,12 @@ export async function load({ url }) {
 	const ips = JSON.parse(await xhe.get('nat_next_ips')) as string[]
 	let peer: Peer | null = null
 	if (url.searchParams.get('peer') === 'share') {
-		let s = await xhe.get('peer.share.decoded')
-		peer = JSON.parse(s)
+		try {
+			let s = await xhe.get('peer.share.decoded')
+			peer = JSON.parse(s)
+		} catch {
+			redirect(307, '/')
+		}
 	}
 	return {
 		status,
