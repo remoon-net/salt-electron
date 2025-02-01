758f7311f59f124bb53d0c2e1806933e8136985e
shynome <shynome@gmail.com>
2025-02-01 22:16:15 +0800

    完善FAQ

--- src/routes/faq/linker.svelte
+++ src/routes/faq/linker.svelte
@@ -5,13 +5,13 @@
 		<dt>wss linker</dt>
 		<dd>
 			连接信令服务提供方提供的 wss 链接以便对外提供服务, 格式为
-			<code>wss://whip-provider.host/path#http://whip-endpoint/path</code>, 一般都是用这个, 比较省心
+			<code>wss://whip-provider.host/path#wss://whip-endpoint/path</code>, 一般都是用这个, 比较省心
 		</dd>
 	</dl>
 	<dl>
 		<dt>http linker</dt>
 		<dd>
-			格式为 <code>http://0.0.0.0:8999#[http://whip-endpoint/path]</code>, 这里的
+			格式为 <code>http://0.0.0.0:8999#[wss://whip-endpoint/path]</code>, 这里的
 			<code>whip-endpoint</code> 一般来说都是反代地址, 只要其它节点能访问到都行
 		</dd>
 	</dl>
