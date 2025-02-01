ee816bebdc9ca6e22d85f976815edbce05e70c71
shynome <shynome@gmail.com>
2025-02-01 12:03:41 +0800

    添加nowsc tag用途说明

--- src/routes/faq/ice-policy.svelte
+++ src/routes/faq/ice-policy.svelte
@@ -9,6 +9,14 @@
 		</dd>
 	</dl>
 	<dl>
+		<dt><code>nowsc</code></dt>
+		<dd>
+			禁止使用 Linker 的 WebSocket 通道辅助建立连接. 默认是启用的,
+			因为可以帮助节点快速建立连接虽然会消耗更多 Linker 的流量. 禁用的话会导致连接时间过长,
+			但可以节省流量
+		</dd>
+	</dl>
+	<dl>
 		<dt><code>relay</code></dt>
 		<dd>所有 ICE Server 默认都有这个 tag, 帮助节点建立连接</dd>
 	</dl>
