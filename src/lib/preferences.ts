7e581af5057c709b541f8a89f595e980851c0c53
shynome <shynome@gmail.com>
2025-02-09 22:28:34 +0800

    electron 端已实现 preferences

--- src/lib/preferences.ts
+++ src/lib/preferences.ts
@@ -1,15 +1,6 @@
-import { Capacitor, registerPlugin } from '@capacitor/core'
 import {
 	Preferences as PreferencesRaw,
 	type PreferencesPlugin as PreferencesPluginRaw,
 } from '@capacitor/preferences'
 
-type PreferencesPlugin = Pick<PreferencesPluginRaw, 'get' | 'set'>
-
-const isElectron = Capacitor.getPlatform() === 'electron'
-
-const PreferencesElectron = registerPlugin<PreferencesPlugin>('xhe-preferences', {
-	electron: () => (window as any).CapacitorCustomPlatform.plugins.XhePreferences,
-})
-
-export const Preferences = isElectron ? PreferencesElectron : PreferencesRaw
+export const Preferences: Pick<PreferencesPluginRaw, 'get' | 'set'> = PreferencesRaw
